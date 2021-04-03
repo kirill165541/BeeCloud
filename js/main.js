89d9bf6a4b8188f9be0ab656fc07d91eca99f0f5
kirill165541 <57109464+kirill165541@users.noreply.github.com>
2021-04-03 22:26:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function dropDownList () {
+function dropDownText () {
 	const faqItem = document.querySelectorAll('.faq-item');
 	const faqItemDesc = document.querySelectorAll('.faq-item-desc');
 	const faqItemP = document.querySelectorAll('.faq-item-p');
@@ -8,25 +8,29 @@ function dropDownList () {
 			faqItemDesc[i].classList.toggle("faq-item-desc-click");
 			faqItemP[i].classList.toggle("faq-item_p-bachground");
 		};
-	}
+	}	
 }
 
 function openCloseBurgerMenu () {
 	const mobileBurger = document.querySelector('.mobile-buger');
 	const burgerMenu = document.querySelector('.mobile-burger-menu');
 	const body = document.querySelector('body');
+	const pageWrap = document.querySelector('.page-wrap');
 	const cross = document.querySelector('.cross');
 
 	mobileBurger.onclick = function () {
 		burgerMenu.classList.add('active');
 		body.classList.add('active');
+		pageWrap.classList.add('active');
 	};
 
 	cross.onclick = function () {
 		burgerMenu.classList.remove('active');
 		body.classList.remove('active');
+		pageWrap.classList.remove('active');
 	};
 }
 
-dropDownList ();
+
+dropDownText ();
 openCloseBurgerMenu ();
