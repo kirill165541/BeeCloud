99f27853c6a0f4a343894a3d93830e95d84ba3be
kirill165541 <57109464+kirill165541@users.noreply.github.com>
2021-04-03 12:29:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-const faqItem = document.querySelectorAll('.faq-item');
-const faqItemDesc = document.querySelectorAll('.faq-item-desc');
-const faqItemP = document.querySelectorAll('.faq-item-p');
+function dropDownList () {
+	const faqItem = document.querySelectorAll('.faq-item');
+	const faqItemDesc = document.querySelectorAll('.faq-item-desc');
+	const faqItemP = document.querySelectorAll('.faq-item-p');
 
-for (let i=0; i < faqItem.length; i++){
-	faqItem[i].onclick = function () {
-		faqItemDesc[i].classList.toggle("faq-item-desc-click");
-		faqItemP[i].classList.toggle("faq-item_p-bachground");
+	for (let i=0; i < faqItem.length; i++){
+		faqItem[i].onclick = function () {
+			faqItemDesc[i].classList.toggle("faq-item-desc-click");
+			faqItemP[i].classList.toggle("faq-item_p-bachground");
+		};
+	}
+}
+
+function openCloseBurgerMenu () {
+	const mobileBurger = document.querySelector('.mobile-buger');
+	const burgerMenu = document.querySelector('.mobile-burger-menu');
+	const body = document.querySelector('body');
+	const cross = document.querySelector('.cross');
+
+	mobileBurger.onclick = function () {
+		burgerMenu.classList.add('active');
+		body.classList.add('active');
+	};
+
+	cross.onclick = function () {
+		burgerMenu.classList.remove('active');
+		body.classList.remove('active');
 	};
 }
 
-const mobileBurger = document.querySelector('.mobile-buger');
-const burgerMenu = document.querySelector('.mobile-burger-menu');
-const body = document.querySelector('body');
-const cross = document.querySelector('.cross');
-
-mobileBurger.onclick = function () {
-	burgerMenu.classList.add('active');
-	body.classList.add('active');
-};
-
-cross.onclick = function () {
-	burgerMenu.classList.remove('active');
-	body.classList.remove('active');
-};
+dropDownList ();
+openCloseBurgerMenu ();
